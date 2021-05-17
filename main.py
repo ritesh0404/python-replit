82a32d927d2e637b03b96001ae5b3d66a62cb040
Ritesh Kumar <>
2021-05-17 18:35:24 +0000

    Ritesh

--- main.py
+++ main.py
@@ -1,2 +1,130 @@
-name = input('whats the name?')
-print('Hello ' + name)
+##name = input('whats the name?')
+
+## To comment multiple line: crtl + /
+
+# name =  'Ritesh'
+# print('Hello ' + name)
+a = 1
+b = 10
+c = 3
+d,e,f = 6,7,8
+
+# print (a+b)
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+##Data Types#
+
+##++++ Fundamental Data Types
+## int
+# print('INT Data Type Results::')
+# print(type(a-b)) #display type
+# print (a-b)
+# print(b ** c ) #b to the power of c
+# print(b // 3) #b devide by 3 : quotient is 
+# print(b % c) # b / c give remainder as
+# print(round(3.4))
+
+## float
+# print('FLOAT Data Type Results::')
+# print(type(a/b))
+# print(a/b)
+# print(float(str(10)))
+
+##complex
+
+##binary
+# print('Binary number Results::')
+# print(bin(5))
+# print(type(bin(5)))
+# print(int('0b101', 2))
+# print(bin(0))
+
+## bool
+# print(type(a/b))
+# print(a/b)
+
+## str
+name1 = 'Ram Narayan'
+name2 = "Sita Narayani"
+# long_name3 = '''Hello
+# Hi
+# How 
+# Are 
+# You??
+#  )O O(
+#    ~
+#   \\//  
+#  ??'''
+# print(name1)
+# print(name2)
+# print(long_name3)
+# string concatenation
+# print (name1 +name2)
+# comb_name = name1 + name2
+# print(comb_name)
+# print(type(str(100)))
+## Escapr Sequence (whatever comes after \ is a string)
+# print('It\'s a wonder full day in \"Delhi.\" ')
+## #Escapr Sequence (add a tab using \t )
+# print('It\'s a wonder \t full day in \"Delhi.\" ')
+## #Escapr Sequence (start new line  \n)
+# print('It\'s a wonder full day in \n"Delhi.\" ')
+## formatted Strings
+# cus_name = 'Radha Krihna'
+# cus_type = 'God'
+# age = 999000009999
+# print(f'Hello {cus_name} ji. \nYour customer type is {cus_type}. \nAs man knows you are at least there from {age} years protecting us. Thank You!') 
+
+## list
+## print(type(a/b))
+## print(a/b)
+
+## tuple
+# print(type(a/b))
+# print(a/b)
+
+## set
+# print(type(a/b))
+# print(a/b)
+
+## dict
+# print(type(a/b))
+# print(a/b)
+
+## +++ Classes (customised data Types)
+
+##Specialised Data Types (from Libs)
+
+##None
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+##Operator Presedence (BODMAS) ##
+# print ('Operator Presedence Results::')
+# print ((20 - 3) * 4 )
+# print (20 - 3 * 4 )
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+## snake_case
+## Variable should be starting with lower case or 
+## underscore (private variable) and dont use
+## key words or othe standards
+
+## two underscore should not be used consequetively
+
+## Constants should be in capitals as standard
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+##Augmented Assignment Operator
+# print('Augmented Assignment Operator Results::')
+# some_value = 2
+# print(some_value)
+# some_value += 5
+# print(some_value)
+# some_value -=10
+# print(some_value)
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
